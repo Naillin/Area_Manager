78d1b45255ada73211bec0d9a886f26e6f6d0d32
Naillin <kamilasd@yandex.ru>
2024-12-03 15:43:46 +0500

    first

---
+++ ElevationAnalyzer.py
@@ -0,0 +1,194 @@
+import logging
+import asyncio
+import aiohttp
+import math
+
+# Инициализация логгера для модуля ElevationAnalyzer
+logger = logging.getLogger('area-manager.ElevationAnalyzer')
+
+class ElevationAnalyzer:
+    async def get_elevation(self, coords, delay_ms=150):
+        url = f"https://api.open-elevation.com/api/v1/lookup?locations={coords[0]},{coords[1]}"
+
+        # Задержка перед запросом
+        await asyncio.sleep(delay_ms / 1000)
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                data = await response.json()
+
+                if data.get('results') and len(data['results']) > 0:
+                    elevation = data['results'][0]['elevation']
+                    logger.info(f"Высота точки {coords}: {elevation}")
+                    return elevation
+                else:
+                    logger.warning('No elevation data found for the given coordinates.')
+                    return None
+
+    def format_coords(self, coords):
+        return f"{coords[0]:.6f},{coords[1]:.6f}"
+
+    def get_neighbors(self, coords, distance=200):
+        lat, lon = coords
+        neighbors = []
+        for d_lat in range(-1, 2):
+            for d_lon in range(-1, 2):
+                if d_lat == 0 and d_lon == 0:
+                    continue
+                new_lat = lat + d_lat * (distance / 111320)
+                new_lon = lon + d_lon * (distance / (111320 * math.cos(math.radians(lat))))
+                neighbors.append((new_lat, new_lon))
+        return neighbors
+
+    def find_depression_area_with_islands(self, center_coords, initial_height, distance=200):
+        points_to_check = [{'coords': center_coords, 'height': initial_height}]
+        checked_points = set()
+        depression_points = set()
+        non_flooded_points = set()
+        perimeter_points = set()
+        included_points = set()
+        islands = []
+        island_id = 0
+
+        def process_point(current_point, current_height):
+            current_key = self.format_coords(current_point)
+            if current_key in checked_points:
+                return
+            checked_points.add(current_key)
+
+            current_elevation = self.get_elevation(current_point)
+
+            if current_elevation < current_height:
+                depression_points.add(current_key)
+                neighbors = self.get_neighbors(current_point)
+                for neighbor in neighbors:
+                    neighbor_key = self.format_coords(neighbor)
+                    if neighbor_key not in checked_points:
+                        points_to_check.append({
+                            'coords': neighbor,
+                            'height': min(current_height, current_elevation)
+                        })
+            else:
+                non_flooded_points.add(current_key)
+
+        while points_to_check:
+            for point in points_to_check:
+                process_point(point['coords'], point['height'])
+            points_to_check = points_to_check[1:]
+
+        for point_key in depression_points:
+            lat, lon = map(float, point_key.split(','))
+            neighbors = self.get_neighbors((lat, lon))
+
+            has_non_flooded_neighbor = False
+
+            for neighbor in neighbors:
+                neighbor_key = self.format_coords(neighbor)
+
+                if neighbor_key not in depression_points:
+                    has_non_flooded_neighbor = True
+
+                    if neighbor_key in non_flooded_points:
+                        perimeter_points.add(neighbor_key)
+                    else:
+                        included_points.add(point_key)
+
+            if not has_non_flooded_neighbor:
+                existing_island = next((island for island in islands if
+                                        any(self.are_neighbors(coord, (lat, lon), 50) for coord in island['coords'])),
+                                       None)
+
+                if existing_island:
+                    existing_island['coords'].append((lat, lon))
+                else:
+                    islands.append({'id': island_id + 1, 'coords': [(lat, lon)]})
+                    island_id += 1
+
+        logger.info("Depression Points: %s", [list(map(float, point.split(','))) for point in depression_points])
+        logger.info("Perimeter Points: %s", [list(map(float, point.split(','))) for point in perimeter_points])
+        logger.info("Included Points: %s", [list(map(float, point.split(','))) for point in included_points])
+        logger.info("Islands: %s", islands)
+        return {
+            'depression_points': [list(map(float, point.split(','))) for point in depression_points],
+            'perimeter_points': [list(map(float, point.split(','))) for point in perimeter_points],
+            'included_points': [list(map(float, point.split(','))) for point in included_points],
+            'islands': islands
+        }
+
+    # async def find_depression_area_with_islands(self, center_coords, initial_height, distance=200):
+    #     points_to_check = [{'coords': center_coords, 'height': initial_height}]
+    #     checked_points = set()
+    #     depression_points = set()
+    #     non_flooded_points = set()
+    #     perimeter_points = set()
+    #     included_points = set()
+    #     islands = []
+    #     island_id = 0
+    #
+    #     async def process_point(current_point, current_height):
+    #         current_key = self.format_coords(current_point)
+    #         if current_key in checked_points:
+    #             return
+    #         checked_points.add(current_key)
+    #
+    #         current_elevation = await self.get_elevation(current_point)
+    #
+    #         if current_elevation < current_height:
+    #             depression_points.add(current_key)
+    #             neighbors = self.get_neighbors(current_point)
+    #             for neighbor in neighbors:
+    #                 neighbor_key = self.format_coords(neighbor)
+    #                 if neighbor_key not in checked_points:
+    #                     points_to_check.append({
+    #                         'coords': neighbor,
+    #                         'height': min(current_height, current_elevation)
+    #                     })
+    #         else:
+    #             non_flooded_points.add(current_key)
+    #
+    #     while points_to_check:
+    #         await asyncio.gather(*[process_point(point['coords'], point['height']) for point in points_to_check])
+    #         points_to_check = points_to_check[1:]
+    #
+    #     for point_key in depression_points:
+    #         lat, lon = map(float, point_key.split(','))
+    #         neighbors = self.get_neighbors((lat, lon))
+    #
+    #         has_non_flooded_neighbor = False
+    #
+    #         for neighbor in neighbors:
+    #             neighbor_key = self.format_coords(neighbor)
+    #
+    #             if neighbor_key not in depression_points:
+    #                 has_non_flooded_neighbor = True
+    #
+    #                 if neighbor_key in non_flooded_points:
+    #                     perimeter_points.add(neighbor_key)
+    #                 else:
+    #                     included_points.add(point_key)
+    #
+    #         if not has_non_flooded_neighbor:
+    #             existing_island = next((island for island in islands if any(self.are_neighbors(coord, (lat, lon), 50) for coord in island['coords'])), None)
+    #
+    #             if existing_island:
+    #                 existing_island['coords'].append((lat, lon))
+    #             else:
+    #                 islands.append({'id': island_id + 1, 'coords': [(lat, lon)]})
+    #                 island_id += 1
+    #
+    #     logger.info("Depression Points: %s", [list(map(float, point.split(','))) for point in depression_points])
+    #     logger.info("Perimeter Points: %s", [list(map(float, point.split(','))) for point in perimeter_points])
+    #     logger.info("Included Points: %s", [list(map(float, point.split(','))) for point in included_points])
+    #     logger.info("Islands: %s", islands)
+    #     return {
+    #         'depression_points': [list(map(float, point.split(','))) for point in depression_points],
+    #         'perimeter_points': [list(map(float, point.split(','))) for point in perimeter_points],
+    #         'included_points': [list(map(float, point.split(','))) for point in included_points],
+    #         'islands': islands
+    #     }
+
+    def are_neighbors(self, coord1, coord2, check_distance):
+        lat1, lon1 = coord1
+        lat2, lon2 = coord2
+        distance = math.sqrt((lat1 - lat2) ** 2 + (lon1 - lon2) ** 2)
+        return distance <= (check_distance / 111320)
---
+++ main.py
@@ -0,0 +1,248 @@
+import sqlite3
+import asyncio
+import time
+from datetime import datetime, timedelta
+import logging
+import platform
+from logging.handlers import SysLogHandler
+from ElevationAnalyzer import ElevationAnalyzer
+
+# Настройка логгера
+logger = logging.getLogger('area-manager.main')
+logger.setLevel(logging.INFO)
+
+if platform.system() == 'Windows':
+    # Логгер для Windows (в файл)
+    file_handler = logging.FileHandler('area-manager.log')
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(message)s'))
+    logger.addHandler(file_handler)
+else:
+    # Логгер для Linux (в syslog через systemd)
+    syslog_handler = SysLogHandler(address='/dev/log')
+    syslog_handler.setFormatter(logging.Formatter('%(name)s: %(message)s'))
+    logger.addHandler(syslog_handler)
+
+def calculate_moving_average(data, window_size = 7):
+    moving_average = []
+
+    # Преобразуем строковые значения в числа
+    data_values = [float(item['Value_Data']) for item in data]
+    logger.debug(f"Data values: {data_values}")
+
+    # Рассчитываем скользящее среднее
+    for i in range(len(data_values)):
+        if i < window_size - 1:
+            moving_average.append({'Value_Data': None, 'Time_Data': data[i]['Time_Data']})  # Недостаточно данных для расчета
+        else:
+            sum_values = sum(data_values[i - window_size + 1:i + 1])
+            average = sum_values / window_size
+            moving_average.append({'Value_Data': average, 'Time_Data': data[i]['Time_Data']})
+            logger.debug(f"Moving average at index {i}: {average}")
+
+    # Предсказываем на 3 дня вперед с учетом тенденции
+    last_values = data_values[-window_size:]
+    last_times = [item['Time_Data'] for item in data[-window_size:]]
+    last_average = sum(last_values) / window_size
+    logger.debug(f"Last average: {last_average}")
+
+    # Рассчитываем средний интервал времени между последними событиями
+    time_intervals = [(last_times[i + 1] - last_times[i]).total_seconds() for i in range(len(last_times) - 1)]
+    average_time_interval = sum(time_intervals) / len(time_intervals)
+    logger.debug(f"Average time interval: {average_time_interval}")
+
+    # Рассчитываем тенденцию (наклон)
+    slope = (last_values[-1] - last_values[0]) / (window_size - 1)
+    logger.debug(f"Slope: {slope}")
+
+    for i in range(3):
+        predicted_value = last_average + slope * (i + 1)
+        predicted_time = last_times[-1] + timedelta(seconds=average_time_interval * (i + 1))
+        moving_average.append({'Value_Data': predicted_value, 'Time_Data': predicted_time})
+        logger.debug(f"Predicted value for day {i + 1}: {predicted_value} at {predicted_time}")
+
+    return moving_average
+
+def check_topic_conditions(topic_id, db_path):
+    conn = sqlite3.connect(db_path)
+    conn.execute('PRAGMA journal_mode=WAL')
+    cursor = conn.cursor()
+
+    # Получаем данные топика
+    cursor.execute("SELECT Altitude_Topic FROM Topic WHERE ID_Topic = ?", (topic_id,))
+    alt = cursor.fetchone()
+    if alt is None:
+        logger.warning(f"Topic with ID {topic_id} not found.")
+        conn.close()
+        return False, None  # Топик не найден
+    alt = alt[0]
+    logger.info(f"Altitude for topic {topic_id}: {alt}")
+
+    # Получаем данные Data для топика
+    cursor.execute("SELECT Value_Data, Time_Data FROM Data WHERE ID_Topic = ? ORDER BY Time_Data DESC", (topic_id,))
+    data = [{'Value_Data': row[0], 'Time_Data': datetime.fromtimestamp(row[1] / 1000)} for row in cursor.fetchall()]
+    conn.close()
+
+    if not data:
+        logger.warning(f"No data found for topic {topic_id}.")
+        return False, None  # Данные по топику отсутствуют
+
+    logger.info(f"Data for topic {topic_id}: {data}")
+
+    # Вычисляем скользящее среднее и предсказываем 3 события
+    predicted_events = calculate_moving_average(data)
+    if len(predicted_events) < 10:
+        logger.warning(f"Not enough data to predict for topic {topic_id}.")
+        return False, None  # Недостаточно данных для предсказания
+
+    p1, p2, p3 = [event['Value_Data'] for event in predicted_events[-3:]]
+    logger.info(f"Predicted values for topic {topic_id}: p1={p1}, p2={p2}, p3={p3}")
+
+    # Определяем последнюю и предпоследнюю фактическую высоту топика
+    if len(data) < 2:
+        logger.warning(f"Not enough data to compare for topic {topic_id}.")
+        return False, p3  # Недостаточно данных для сравнения
+
+    f1, f2 = float(data[0]['Value_Data']), float(data[1]['Value_Data'])
+    logger.info(f"Actual values for topic {topic_id}: f1={f1}, f2={f2}")
+
+    # Проверяем условия
+    if p3 > alt and f1 < f2:
+        logger.info(f"Conditions met for topic {topic_id}: p3={p3} > alt={alt} and f1={f1} < f2={f2}")
+        return True, p3
+    else:
+        logger.info(f"Conditions not met for topic {topic_id}: p3={p3} > alt={alt} and f1={f1} < f2={f2}")
+        return False, p3
+
+def main():
+    db_path = '../MQTT_Data_collector/mqtt_data.db'
+    analyzer = ElevationAnalyzer()
+
+    while True:
+        # Получаем все топики
+        with sqlite3.connect(db_path) as conn:
+            conn.execute('PRAGMA journal_mode=WAL')
+            cursor = conn.cursor()
+            cursor.execute("SELECT ID_Topic, Latitude_Topic, Longitude_Topic, CheckTime_Topic FROM Topic")
+            topics = cursor.fetchall()
+
+        for topic in topics:
+            topic_id, latitude, longitude, check_time = topic
+            logger.info(f"Checking topic {topic_id}")
+
+            # Проверяем, когда был последний расчет
+            if check_time is None or (datetime.now() - datetime.fromisoformat(check_time)).total_seconds() >= 2 * 3600:
+                # Открываем соединение для проверки условий
+                with sqlite3.connect(db_path) as conn:
+                    conn.execute('PRAGMA journal_mode=WAL')
+                    cursor = conn.cursor()
+                    conditions_met, p3 = check_topic_conditions(topic_id, db_path)
+
+                if conditions_met:
+                    logger.info(f"Conditions met for topic {topic_id}. Calculating area points.")
+
+                    center_coords = (latitude, longitude)
+                    initial_height = p3  # Используем последнее предсказанное значение (p3)
+
+                    # Вычисляем точки
+                    result = analyzer.find_depression_area_with_islands(center_coords, initial_height)
+
+                    # Открываем соединение для записи данных
+                    with sqlite3.connect(db_path) as conn:
+                        conn.execute('PRAGMA journal_mode=WAL')
+                        cursor = conn.cursor()
+
+                        # Удаляем старые данные для топика
+                        cursor.execute("""
+                            DELETE FROM AreaPoints WHERE ID_Topic = ?
+                        """, (topic_id,))
+
+                        # Записываем новые данные в таблицу AreaPoints
+                        cursor.execute("""
+                            INSERT INTO AreaPoints (ID_Topic, Depression_AreaPoint, Perimeter_AreaPoint, Included_AreaPoint, Islands_AreaPoint)
+                            VALUES (?, ?, ?, ?, ?)
+                        """, (topic_id, str(result['depression_points']), str(result['perimeter_points']),
+                              str(result['included_points']), str(result['islands'])))
+
+                        # Обновляем CheckTime_Topic
+                        cursor.execute("""
+                            UPDATE Topic SET CheckTime_Topic = ? WHERE ID_Topic = ?
+                        """, (datetime.now().timestamp(), topic_id))
+
+                        conn.commit()
+                        logger.info(f"Data for topic {topic_id} inserted into AreaPoints and CheckTime_Topic updated.")
+                else:
+                    logger.info(f"Conditions not met for topic {topic_id}. Skipping calculation.")
+            else:
+                logger.info(f"Topic {topic_id} was recently checked. Skipping calculation.")
+
+        time.sleep(60)  # Пауза перед следующей итерацией
+
+# Запуск функции
+main()
+
+# async def main(): # убрать асинхронность
+#     db_path = '../MQTT_Data_collector/mqtt_data.db'
+#     analyzer = ElevationAnalyzer()
+#
+#     while True:
+#         # Получаем все топики
+#         with sqlite3.connect(db_path) as conn:
+#             conn.execute('PRAGMA journal_mode=WAL')
+#             cursor = conn.cursor()
+#             cursor.execute("SELECT ID_Topic, Latitude_Topic, Longitude_Topic, CheckTime_Topic FROM Topic")
+#             topics = cursor.fetchall()
+#
+#         for topic in topics:
+#             topic_id, latitude, longitude, check_time = topic
+#             logger.info(f"Checking topic {topic_id}")
+#
+#             # Проверяем, когда был последний расчет
+#             if check_time is None or (datetime.now() - datetime.fromisoformat(check_time)).total_seconds() >= 2 * 3600:
+#                 # Открываем соединение для проверки условий
+#                 with sqlite3.connect(db_path) as conn:
+#                     conn.execute('PRAGMA journal_mode=WAL')
+#                     cursor = conn.cursor()
+#                     conditions_met, p3 = check_topic_conditions(topic_id, db_path)
+#
+#                 if conditions_met:
+#                     logger.info(f"Conditions met for topic {topic_id}. Calculating area points.")
+#
+#                     center_coords = (latitude, longitude)
+#                     initial_height = p3  # Используем последнее предсказанное значение (p3)
+#
+#                     # Вычисляем точки
+#                     result = await analyzer.find_depression_area_with_islands(center_coords, initial_height)
+#
+#                     # Открываем соединение для записи данных
+#                     with sqlite3.connect(db_path) as conn:
+#                         conn.execute('PRAGMA journal_mode=WAL')
+#                         cursor = conn.cursor()
+#
+#                         # Удаляем старые данные для топика
+#                         cursor.execute("""
+#                             DELETE FROM AreaPoints WHERE ID_Topic = ?
+#                         """, (topic_id,))
+#
+#                         # Записываем новые данные в таблицу AreaPoints
+#                         cursor.execute("""
+#                             INSERT INTO AreaPoints (ID_Topic, Depression_AreaPoint, Perimeter_AreaPoint, Included_AreaPoint, Islands_AreaPoint)
+#                             VALUES (?, ?, ?, ?, ?)
+#                         """, (topic_id, str(result['depression_points']), str(result['perimeter_points']),
+#                               str(result['included_points']), str(result['islands']))) #понять как работае
+#
+#                         # Обновляем CheckTime_Topic
+#                         cursor.execute("""
+#                             UPDATE Topic SET CheckTime_Topic = ? WHERE ID_Topic = ?
+#                         """, (datetime.now().timestamp(), topic_id))
+#
+#                         conn.commit()
+#                         logger.info(f"Data for topic {topic_id} inserted into AreaPoints and CheckTime_Topic updated.")
+#                 else:
+#                     logger.info(f"Conditions not met for topic {topic_id}. Skipping calculation.")
+#             else:
+#                 logger.info(f"Topic {topic_id} was recently checked. Skipping calculation.")
+#
+#         await asyncio.sleep(60)  # Пауза перед следующей итерацией
+#
+# # Запуск асинхронной функции
+# asyncio.run(main())
